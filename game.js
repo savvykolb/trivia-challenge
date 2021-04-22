030c1ef831e9b1061468f5067a41fbd1930a3674
Savannah Kolb <savannahnkolb@gmail.com>
2021-04-21 20:54:55 -0400

    fixed console errors/syntax errors. working on timer. Attempt 1 did not work

--- game.js
+++ game.js
@@ -16,68 +16,70 @@ let questions = [
         choice2: 'October',
         choice3: 'January',
         choice4: 'April',
-        answer: 3,
-    }
+        answer: 3
+    },
     {
         question: 'Which nut is used to make dynamite?',
         choice1: 'Almonds',
         choice2: 'Pine nuts',
         choice3: 'Walnuts',
         choice4: 'Peanuts',
-        answer: 4,
-    }
+        answer: 4
+    },
     {
         question: 'What is a group of turkeys called?',
         choice1: 'A clutch',
         choice2: 'A rafter',
         choice3: 'A brood',
         choice4: 'A peep',
-        answer: 2,
-    }
+        answer: 2
+    },
     {
         question: 'What was the first car that was mass-produced?',
         choice1: 'Ford Ram',
         choice2: 'Ford Model-A',
         choice3: 'Ford Model-T',
         choice4: 'Ford Truck',
-        answer: 3,
-    }
+        answer: 3
+    },
     {
         question: 'What is the sum of the angles of a triangle?',
         choice1: '360',
         choice2: '190',
         choice3: '370',
         choice4: '180',
-        answer: 4,
+        answer: 4
     }
 ]
 
-const SCORE_POINTS = 25;
-const MAX_QUESTIONS = 5;
-let countdown = 60;
-
 const SCORE_POINTS = 100;
 const MAX_QUESTIONS = 5;
+let countdown = 60;
 
 var startGame = function () {
     questionCounter = 0;
     score = 0;
-    availableQuestions = (...questions);
+    availableQuestions = [...questions],
     getNewQuestion();
 }
 
-function countdown() {
-    let timeInterval = setInterval(function () {
-      if (countDown > 1) {
-        timeLeft.textContent = "Timer : " + countDown;
-        countDown--;
+var countdown = function () {
+    var timeLeft = 75;
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+      // As long as the `timeLeft` is greater than 1
+      if (timeLeft > 1) {
+        // Set the `textContent` of `timerEl` to show the remaining seconds
+        timerEl.textContent = "Timer: " + timeLeft;
+        // Decrement `timeLeft` by 1
+        timeLeft--;
       } else {
-        timeLeft.textContent = 'Time is up!';
+        // Once `timeLeft` gets to 0, set `timerEl` to 'Time is up!'
+        timerEl.textContent = 'Time is up!';
+        // Use `clearInterval()` to stop the timer
         clearInterval(timeInterval);
-        return window.location.assign("../html/end.html");
         }
     }, 1000);
-}
 
 var getNewQuestion = function() {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
@@ -105,20 +107,20 @@ var getNewQuestion = function() {
 }
 
 choices.forEach(function(choice) {
-    choice.addEventListener("click", function(event) {
+    choice.addEventListener('click', function(event) {
         if(!acceptingAnswer) return;
         
         acceptingAnswer = false; 
         const selectedChoice = event.target; 
-        const selectedAnswer = selectedChoice.dataset["number"]; 
+        const selectedAnswer = selectedChoice.dataset['number']; 
 
-        let classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
-        if(classToApply === "correct") {
+        if(classToApply === 'correct') {
             incrementScore(SCORE_POINTS)
         } else {
             countDown-=10;
-            timeLeft.textContent = "Timer : " + countDown;
+            timeLeft.textContent = 'Timer : ' + countDown;
         }
 
         selectedChoice.parentElement.classList.add(classToApply);
